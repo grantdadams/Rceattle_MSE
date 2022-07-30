f8e3fedf50a346f659d691aa08ed56873f01c6bf
Grant Adams <grantadams60091@gmail.com>
2022-07-30 14:01:43 -0700

    ebs plots

--- R/BSAI_MSE_Run_summary.R
+++ R/BSAI_MSE_Run_summary.R
@@ -53,10 +53,10 @@ em_hcr_list_estM = c(em_hcr_list_estM, em_hcr_list_estM)
 em_hcr_names <- c("SS_fixM_Tier3_EM", "SS_fixM_dynamicTier3_EM", "SS_fixM_Cat1_EM", "SS_fixM_dynamicCat1_EM", "SS_fixM_Tier1_EM", "SS_fixM_dynamicTier1_EM", "SS_fixM_Fspr_EM", "SS_fixM_AvgF_EM", # Fixed M
                   "SS_estM_Tier3_EM", "SS_estM_dynamicTier3_EM", "SS_estM_Cat1_EM", "SS_estM_dynamicCat1_EM", "SS_estM_Tier1_EM", "SS_estM_dynamicTier1_EM", "SS_estM_Fspr_EM", "SS_estM_AvgF_EM")
 
-plot_biomass(om_list, file = "Results/Figures/EBS_OM_", model_names = om_names)
-plot_ssb(om_list, file = "Results/Figures/EBS_OM_", model_names = om_names)
-plot_recruitment(om_list, file = "Results/Figures/EBS_OM_", model_names = om_names)
-
+plot_biomass(projected_OM_no_F, file = "Results/Figures/EBS_OM_", model_names = om_names, width = 6, height = 5)
+plot_ssb(projected_OM_no_F, file = "Results/Figures/EBS_OM_", model_names = om_names, width = 6, height = 5)
+plot_recruitment(projected_OM_no_F, file = "Results/Figures/EBS_OM_", model_names = om_names, width = 6, height = 5)
+plot_b_eaten_prop(projected_OM_no_F, file = "Results/Figures/EBS_OM_", model_names = om_names, width = 6, height = 5)
 
 ################################################
 # Load and run summary
