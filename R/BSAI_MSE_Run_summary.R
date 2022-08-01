2fb0b8a7f9772dfc6738e45107adbd82a0cf219d
Grant Adams <grantadams60091@gmail.com>
2022-08-01 06:54:01 -0700

    Update BSAI_MSE_Run_summary.R

--- R/BSAI_MSE_Run_summary.R
+++ R/BSAI_MSE_Run_summary.R
@@ -61,90 +61,17 @@ plot_b_eaten_prop(projected_OM_no_F, file = "Results/Figures/EBS_OM_", model_nam
 ################################################
 # Load and run summary
 ################################################
-## Loop across OMs,
-for(om in 1:length(om_list)){  # OM model
-  for(em in 1:length(em_hcr_list)){ # EM and HCR
-    
-    # STEP 1 -- Load MSE
-    mse3 <- load_mse(dir = paste0("Runs/EBS/", om_names[om],"/", em_hcr_names[em],"/ConstantR/No cap"), file = NULL)
-    MSE_names <- paste0(om_names[om],"__", em_hcr_names[em])
-    
-    
-    # STEP 2 -- Update Ftarget and Flimit for OMs
-    for(j in 1:length(mse3)){
-      
-      # - Ftarget and Flimit for single-species models
-      if(mse3[[j]]$OM$data_list$msmMode == 0){
-        
-        # -- Fix M
-        if(unique(mse3[[j]]$OM$data_list$est_M1) == 0){
-          mse3[[j]]$OM$data_list$Plimit <- em_hcr_list_fixM[[em]]$data_list$Plimit # Update Target
-          mse3[[j]]$OM$data_list$Ptarget <- em_hcr_list_fixM[[em]]$data_list$Ptarget # Update Limit
-          
-          mse3[[j]]$OM$quantities$Flimit <- em_hcr_list_fixM[[em]]$quantities$Flimit # Update Flimit from Ftarget that was optimized
-          mse3[[j]]$OM$quantities$Ftarget <- em_hcr_list_fixM[[em]]$quantities$Ftarget # Update Flimit from Ftarget that was optimized
-        }
-        
-        # -- Estimate M
-        if(unique(mse3[[j]]$OM$data_list$est_M1) == 1){
-          # - Minus 8 because fix m is the first om in the list and there are 8 hcrs
-          mse3[[j]]$OM$data_list$Plimit <- em_hcr_list_estM[[em]]$data_list$Plimit # Update Target
-          mse3[[j]]$OM$data_list$Ptarget <- em_hcr_list_estM[[em]]$data_list$Ptarget # Update Limit
-          
-          mse3[[j]]$OM$quantities$Flimit <- em_hcr_list_estM[[em]]$quantities$Flimit # Update Flimit from Ftarget that was optimized
-          mse3[[j]]$OM$quantities$Ftarget <- em_hcr_list_estM[[em]]$quantities$Ftarget # Update Flimit from Ftarget that was optimized
-        }
-      }
-      
-      
-      # - Calculate depletion for multi-species models
-      if(mse3[[j]]$OM$data_list$msmMode == 1){
-        mse3[[j]]$OM$quantities$depletionSSB <- mse3[[j]]$OM$quantities$biomassSSB / ms_run$quantities$biomassSSB[,ncol(ms_run$quantities$biomassSSB)] # Divide ssb by SSB in 2060 under no fishing
-        
-        mse3[[j]]$OM$quantities$SB0 <- ms_run$quantities$biomassSSB[,ncol(ms_run$quantities$biomassSSB)] # Update SB0
-        
-        mse3[[j]]$OM$data_list$Plimit <- 0.25 # Update Target
-        mse3[[j]]$OM$data_list$Ptarget <- 0.40 # Update Limit
-        
-        mse3[[j]]$OM$quantities$Flimit <- ms_run_f25$quantities$Ftarget # Update Flimit from Ftarget that was optimized
-        mse3[[j]]$OM$quantities$Ftarget <- ms_run_f25$quantities$Ftarget # Update Flimit from Ftarget that was optimized
-      }
-    }
-    
-    # STEP 3 - Performance metrics
-    mse_metrics <- mse_summary(mse3)
-    mse_metrics <- mse_metrics[1:3,-c(2:3)]
-    mse_metrics <- pivot_longer(mse_metrics, cols = 2:ncol(mse_metrics))
-    colnames(mse_metrics) <- c("Species", "Performance metric", MSE_names)
+# Do summary
+source("R/Run_MSE_summary.R")
 
-    if(om == 1 & em == 1){mse_metrics_complete = mse_metrics}
-    if(om != 1 | em != 1){mse_metrics_complete = cbind(mse_metrics_complete, mse_metrics[,-c(1,2)])}
-    write.csv(mse_metrics, file = paste0("Results/Tables/EBS/EBS_table_", MSE_names,".csv"))
+# - No rec trend
+summary_fun(system = "EBS", recname = "ConstantR", om_list = om_list, projected_OM_no_F = projected_OM_no_F, om_names = om_names, em_hcr_list_fixM = em_hcr_list_fixM, em_hcr_list_estM = em_hcr_list_estM, em_hcr_names = em_hcr_names)
 
 
-    # STEP 4 - Plot
-    plot_depletionSSB(mse3, mse = TRUE, OM = TRUE, file = paste0("Results/Figures/Depletion/EBS true ", MSE_names), line_col  = "#04395E", reference = projected_OM_no_F[[om]], top_adj = 1, species = c(1, 2, 3), width = 4.3, height = 4)
-    plot_depletionSSB(mse3, mse = TRUE, OM = FALSE, file = paste0("Results/Figures/Depletion/EBS Perceived ", MSE_names), line_col = "#5F0F40", top_adj = 1, species = c(1, 2, 3), width = 4.3, height = 4)
+# - ATF Up and Down
+summary_fun(system = "EBS", recname = c("ATFRup", "ATFRdown"), om_list = om_list, projected_OM_no_F = projected_OM_no_F, om_names = om_names, em_hcr_list_fixM = em_hcr_list[c(1:2,1:2)], em_hcr_list_estM = em_hcr_list[c(9:10,9:10)], em_hcr_names = em_hcr_names[c(1:2,9:10)])
 
-    plot_depletionSSB(mse3$Sim_1$EM, mse = FALSE, incl_proj = TRUE, file = paste0("Results/Figures/Depletion/EBS 1 Sim/EBS Perceived 1-Sim ", MSE_names), species = c(1, 2, 3), width = 4.3, height = 4)
 
-    plot_ssb(mse3, mse = TRUE, OM = TRUE, file = paste0("Results/Figures/SSB/EBS true ", MSE_names), line_col  = "#04395E", reference = projected_OM_no_F[[om]], species = c(1, 2, 3), width = 4.3, height = 4)
-    plot_ssb(mse3, mse = TRUE, OM = FALSE, file = paste0("Results/Figures/SSB/EBS Perceived ", MSE_names), line_col = "#5F0F40", species = c(1, 2, 3))
+# - All Up and Down
+summary_fun(system = "EBS", recname = c("AllUp", "AllDown"), om_list = om_list, projected_OM_no_F = projected_OM_no_F, om_names = om_names, em_hcr_list_fixM = em_hcr_list[c(1:2,1:2)], em_hcr_list_estM = em_hcr_list[c(9:10,9:10)], em_hcr_names = em_hcr_names[c(1:2,9:10)])
 
-    plot_biomass(mse3, mse = TRUE, OM = TRUE, file = paste0("Results/Figures/B/EBS true ", MSE_names), line_col  = "#04395E", reference = projected_OM_no_F[[om]], species = c(1, 2, 3), width = 4.3, height = 4)
-    plot_biomass(mse3, mse = TRUE, OM = FALSE, file = paste0("Results/Figures/B/EBS Perceived ", MSE_names), line_col = "#5F0F40", species = c(1, 2, 3), width = 4.3, height = 4)
-
-    plot_recruitment(mse3, mse = TRUE, OM = TRUE, file = paste0("Results/Figures/R/EBS true ", MSE_names), line_col  = "#04395E", species = c(1, 2, 3), width = 4.3, height = 4)
-    plot_recruitment(mse3, mse = TRUE, OM = FALSE, file = paste0("Results/Figures/R/EBS Perceived ", MSE_names), line_col = "#5F0F40", species = c(1, 2, 3), width = 4.3, height = 4)
-
-    plot_f(mse3, mse = TRUE, OM = TRUE, file = paste0("Results/Figures/F/EBS true ", MSE_names), line_col  = "#04395E", species = c(1, 2, 3), width = 4.3, height = 4)
-    plot_f(mse3, mse = TRUE, OM = FALSE, file = paste0("Results/Figures/F/EBS Perceived ", MSE_names), line_col  = "#5F0F40", species = c(1, 2, 3), width = 4.3, height = 4)
-    
-    plot_catch(mse3, mse = TRUE, file = paste0("Results/Figures/Catch/EBS true ", MSE_names), line_col  = "#04395E", ymax = c(1e7, 280000, 1e5), width = 4.3, height = 4)
-    
-    # - Unload for memory
-    rm(mse3)
-  }
-}
-
-write.csv(mse_metrics_complete, file = paste0("Results/Tables/EBS_table_full.csv"))
