1ddeb3904a644740e0c82cbca96e24ecdca12e9b
Grant Adams <grantadams60091@gmail.com>
2022-08-01 07:11:38 -0700

    Update Run_MSE_summary.R

--- R/Functions/Run_MSE_summary.R
+++ R/Functions/Run_MSE_summary.R
@@ -105,28 +105,28 @@ summary_fun <- function(system = "GOA1977", recname = "ConstantR", om_list = NUL
         
         if(om == 1 & em == 1){mse_metrics_complete = mse_metrics}
         if(om != 1 | em != 1){mse_metrics_complete = cbind(mse_metrics_complete, mse_metrics[,-c(1,2)])}
-        write.csv(mse_metrics, file = paste0("Results/",system, "_", recname[rec] "_table", MSE_names,".csv"))
+        write.csv(mse_metrics, file = paste0("Results/",system, "_", recname[rec], "_table", MSE_names,".csv"))
         
         
         # STEP 4 - Plot
-        plot_depletionSSB(mse3, mse = TRUE, OM = TRUE, file = paste0("Results/Figures/Depletion/", system, "_", recname[rec] " true ", MSE_names), line_col  = "#04395E", reference = projected_OM_no_F[[om]], top_adj = 1, species = c(1,3,2), width = 4.3, height = 4)
-        plot_depletionSSB(mse3, mse = TRUE, OM = FALSE, file = paste0("Results/Figures/Depletion/", system, "_", recname[rec] " Perceived ", MSE_names), line_col = "#5F0F40", top_adj = 1, species = c(1,3,2), width = 4.3, height = 4)
+        plot_depletionSSB(mse3, mse = TRUE, OM = TRUE, file = paste0("Results/Figures/Depletion/", system, "_", recname[rec], " true ", MSE_names), line_col  = "#04395E", reference = projected_OM_no_F[[om]], top_adj = 1, species = c(1,3,2), width = 4.3, height = 4)
+        plot_depletionSSB(mse3, mse = TRUE, OM = FALSE, file = paste0("Results/Figures/Depletion/", system, "_", recname[rec], " Perceived ", MSE_names), line_col = "#5F0F40", top_adj = 1, species = c(1,3,2), width = 4.3, height = 4)
         
-        plot_depletionSSB(mse3$Sim_1$EM, mse = FALSE, incl_proj = TRUE, file = paste0("Results/Figures/Depletion/", system, "_", recname[rec] " 1 Sim/", system, " Perceived 1-Sim ", MSE_names), species = c(1,3,2), width = 4.3, height = 4)
+        plot_depletionSSB(mse3$Sim_1$EM, mse = FALSE, incl_proj = TRUE, file = paste0("Results/Figures/Depletion/", system, "_", recname[rec], " 1 Sim/", system, " Perceived 1-Sim ", MSE_names), species = c(1,3,2), width = 4.3, height = 4)
         
-        plot_ssb(mse3, mse = TRUE, OM = TRUE, file = paste0("Results/Figures/SSB/", system, "_", recname[rec] " true ", MSE_names), line_col  = "#04395E", reference = projected_OM_no_F[[om]], species = c(1,3,2), width = 4.3, height = 4)
-        plot_ssb(mse3, mse = TRUE, OM = FALSE, file = paste0("Results/Figures/SSB/", system, "_", recname[rec] " Perceived ", MSE_names), line_col = "#5F0F40", species = c(1,3,2))
+        plot_ssb(mse3, mse = TRUE, OM = TRUE, file = paste0("Results/Figures/SSB/", system, "_", recname[rec], " true ", MSE_names), line_col  = "#04395E", reference = projected_OM_no_F[[om]], species = c(1,3,2), width = 4.3, height = 4)
+        plot_ssb(mse3, mse = TRUE, OM = FALSE, file = paste0("Results/Figures/SSB/", system, "_", recname[rec], " Perceived ", MSE_names), line_col = "#5F0F40", species = c(1,3,2))
         
-        plot_biomass(mse3, mse = TRUE, OM = TRUE, file = paste0("Results/Figures/B/", system, "_", recname[rec] " true ", MSE_names), line_col  = "#04395E", reference = projected_OM_no_F[[om]], species = c(1,3,2), width = 4.3, height = 4)
-        plot_biomass(mse3, mse = TRUE, OM = FALSE, file = paste0("Results/Figures/B/", system, "_", recname[rec] " Perceived ", MSE_names), line_col = "#5F0F40", species = c(1,3,2), width = 4.3, height = 4)
+        plot_biomass(mse3, mse = TRUE, OM = TRUE, file = paste0("Results/Figures/B/", system, "_", recname[rec], " true ", MSE_names), line_col  = "#04395E", reference = projected_OM_no_F[[om]], species = c(1,3,2), width = 4.3, height = 4)
+        plot_biomass(mse3, mse = TRUE, OM = FALSE, file = paste0("Results/Figures/B/", system, "_", recname[rec], " Perceived ", MSE_names), line_col = "#5F0F40", species = c(1,3,2), width = 4.3, height = 4)
         
-        plot_recruitment(mse3, mse = TRUE, OM = TRUE, file = paste0("Results/Figures/R/", system, "_", recname[rec] " true ", MSE_names), line_col  = "#04395E", species = c(1,3,2), width = 4.3, height = 4)
-        plot_recruitment(mse3, mse = TRUE, OM = FALSE, file = paste0("Results/Figures/R/", system, "_", recname[rec] " Perceived ", MSE_names), line_col = "#5F0F40", species = c(1,3,2), width = 4.3, height = 4)
+        plot_recruitment(mse3, mse = TRUE, OM = TRUE, file = paste0("Results/Figures/R/", system, "_", recname[rec], " true ", MSE_names), line_col  = "#04395E", species = c(1,3,2), width = 4.3, height = 4)
+        plot_recruitment(mse3, mse = TRUE, OM = FALSE, file = paste0("Results/Figures/R/", system, "_", recname[rec], " Perceived ", MSE_names), line_col = "#5F0F40", species = c(1,3,2), width = 4.3, height = 4)
         
-        plot_f(mse3, mse = TRUE, OM = TRUE, file = paste0("Results/Figures/F/",system, "_", recname[rec] " true ", MSE_names), line_col  = "#04395E", species = c(1,3,2), width = 4.3, height = 4)
-        plot_f(mse3, mse = TRUE, OM = FALSE, file = paste0("Results/Figures/F/",system, "_", recname[rec] " Perceived ", MSE_names), line_col  = "#5F0F40", species = c(1,3,2), width = 4.3, height = 4)
+        plot_f(mse3, mse = TRUE, OM = TRUE, file = paste0("Results/Figures/F/",system, "_", recname[rec], " true ", MSE_names), line_col  = "#04395E", species = c(1,3,2), width = 4.3, height = 4)
+        plot_f(mse3, mse = TRUE, OM = FALSE, file = paste0("Results/Figures/F/",system, "_", recname[rec], " Perceived ", MSE_names), line_col  = "#5F0F40", species = c(1,3,2), width = 4.3, height = 4)
         
-        plot_catch(mse3, mse = TRUE, file = paste0("Results/Figures/Catch/",system, "_", recname[rec] " true ", MSE_names), line_col  = "#04395E", ymax = c(1500000, 180000, 70000, 32000, 120000), width = 4.3, height = 4)
+        plot_catch(mse3, mse = TRUE, file = paste0("Results/Figures/Catch/",system, "_", recname[rec], " true ", MSE_names), line_col  = "#04395E", ymax = c(1500000, 180000, 70000, 32000, 120000), width = 4.3, height = 4)
         # 
         # - Unload for memory
         rm(mse3)
