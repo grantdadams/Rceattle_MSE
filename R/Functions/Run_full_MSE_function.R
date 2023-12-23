40cbb5901fcb7ba50def7de19fe1c9e23c9a7515
Grant Adams <grantadams60091@gmail.com>
2023-12-23 11:41:19 -0800

    Update Run_full_MSE_function.R

--- R/Functions/Run_full_MSE_function.R
+++ R/Functions/Run_full_MSE_function.R
@@ -41,7 +41,7 @@ run_mse <- function(system = "GOA1977", recname = "ConstantR", om_list = NULL, o
             map =  NULL,
             bounds = NULL,
             file = NULL,
-            estimateMode = 2, # Run projection only
+            estimateMode = 0, # Run projection only
             HCR = build_hcr(HCR = em_hcr_list[[em]]$data_list$HCR, # Tier3 HCR
                             DynamicHCR = em_hcr_list[[em]]$data_list$DynamicHCR,
                             FsprTarget = em_hcr_list[[em]]$data_list$FsprTarget,
