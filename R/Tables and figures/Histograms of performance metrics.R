256e5db6f9c191fbaa0ac8d9d4928bfccdb80a2d
grantdadams <grantadams60091@gmail.com>
2024-01-16 10:48:50 -0800

    clean up files

--- R/Tables and figures/Histograms of performance metrics.R
+++ R/Tables and figures/Histograms of performance metrics.R
@@ -4,6 +4,8 @@
 library(fmsb)
 library(gmRi) # devtools::install_github("https://github.com/gulfofmaine/gmRi")
 library(scales)
+library(tidyr)
+library(dplyr)
 source("R/Functions/Summary_table_functions.R")
 source("R/Functions/Summary_histogram_function.R")
 
@@ -53,13 +55,24 @@ mse_histogram(system = "GOA", species = "Arrowtooth flounder", file = "Results/F
 
 
 ## Table of PMs for specific EMs ----
+
+pm_names <- c("Average Catch", "Catch IAV", "P(Closed)", "Avg terminal SSB Relative MSE" , "EM: P(Fy > Flimit)"  , "EM: P(SSB < SSBlimit)" , "OM: P(Fy > Flimit)", "OM: P(SSB < SSBlimit)" , "OM: Terminal SSB Depletion") # Names in table
+
 # - Tier 3
 ind <- 1
 om_names = c("SS_OM", "SS_Ricker_OM", "SSM_OM", "SSM_Ricker_OM", "MS_OM", "MS_Ricker_OM")
 EM_names <- c("SS_fixM_Tier3_EM", "SS_estM_Tier3_EM")
 output_table = pm_summary_table(om_names, EM_names, recname = recname)
-write.csv(output_table$EBS, file = paste0("Results/Tables/TableS", ind,"a_EBS_", recname,"_Tier3_summary.csv"))
-write.csv(output_table$GOA, file = paste0("Results/Tables/TableS", ind,"c_GOA_", recname,"_Tier3_summary.csv"))
+
+output_EBS <- output_table$EBS %>%
+  filter(Performance.metric %in% pm_names) %>%
+  pivot_wider(names_from = c(OM, EM), values_from = Value)
+write.csv(output_EBS, file = paste0("Results/Tables/TableS", ind,"a_EBS_", recname,"_Tier3_summary.csv"))
+
+output_GOA <- output_table$GOA %>%
+  filter(Performance.metric %in% pm_names) %>%
+  pivot_wider(names_from = c(OM, EM), values_from = Value)
+write.csv(output_GOA, file = paste0("Results/Tables/TableS", ind,"c_GOA_", recname,"_Tier3_summary.csv"))
 
 # - Dynamic Tier 3
 EM_names <- c("SS_fixM_dynamicTier3_EM", "SS_estM_dynamicTier3_EM")
