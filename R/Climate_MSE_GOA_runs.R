21a6c267b60773f0dc4aa0dcd8ab3179ac713ad0
Grant Adams <grantadams60091@gmail.com>
2024-03-02 10:45:20 -0800

    Update Climate_MSE_GOA_runs.R

--- R/Climate_MSE_GOA_runs.R
+++ R/Climate_MSE_GOA_runs.R
@@ -5,17 +5,24 @@ source("R/Climate_MSE_condition_GOA_OMs.R")
 source("R/Climate_MSE_condition_GOA_EMs.R")
 
 ## Cap
-# 1. Max historical catch for Arrowtooth flounder
-# 2. No cap
+# - Max historical catch for Arrowtooth flounder
 max_atf <- ss_mod$data_list$fsh_biom
 max_atf <- max_atf[which(max_atf$Species == 2),]
 
-# Pollock, cod, atf
 cap_list <-  c(1e10, max(max_atf$Catch, na.rm = TRUE), 1e10)
+
+# - Max historical catch for all species
+max_catch <- ss_mod$data_list$fsh_biom %>%
+  filter(Year < 2024) %>%
+  group_by(Year, Species) %>%
+  summarise(Catch = sum(Catch)) %>%
+  group_by(Species) %>%
+  summarise(MaxCatch = max(Catch))
+
+cap_list <-  max_catch$MaxCatch
 
 ## Sampling period
 sampling_period <- c(2,2,1,2,2,2,2,1,2,2,1,2,2,1,1,1,1,1)
-
 
 ################################################
 # Management strategy evaluation
@@ -24,12 +31,12 @@ sampling_period <- c(2,2,1,2,2,2,2,1,2,2,1,2,2,1,1,1,1,1)
 source("R/Functions/Run_climate_MSE_function.R")
 
 # No cap
-run_climate_mse(system = "GOA_Climate", om_list = om_list[-c(1:4)], om_names = om_names[-c(1:4)], em_hcr_list = em_list[8], em_hcr_names = em_names[8], sampling_period = sampling_period, nsim = 200, regenerate_past = FALSE, cap = NULL)
+run_climate_mse(system = "GOA_Climate", om_list = om_list, om_names = om_names, em_hcr_list = em_list[7], em_hcr_names = em_names[7], sampling_period = sampling_period, nsim = 200, regenerate_past = FALSE, cap = NULL)
 
 # Cap
-run_climate_mse(system = "GOA_Climate", om_list = om_list, om_names = om_names, em_hcr_list = em_list[8], em_hcr_names = em_names[8], sampling_period = sampling_period, nsim = 200, regenerate_past = FALSE, cap = cap_list)
+run_climate_mse(system = "GOA_Climate", om_list = om_list, om_names = om_names, em_hcr_list = em_list[7], em_hcr_names = em_names[7], sampling_period = sampling_period, nsim = 200, regenerate_past = FALSE, cap = cap_list)
 
 
-# 12 = HCRs 1/2, 1 = HCR 3/4, 3 = HCR 5, 4 = HCR 6, 5 = HCR 7, 8 =  HCR 8
+# 12 = HCRs 1/2, 1 = HCR 3/4, 3 = HCR 5, 4 = HCR 6, 5,9,10 = HCR 7, 8 =  HCR 8
 
 
