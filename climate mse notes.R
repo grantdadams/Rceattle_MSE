23e692af7c97e075555f3ec314bbb1dae494e846
Grant Adams <grantadams60091@gmail.com>
2024-04-20 09:55:28 -0700

    update plot catch for projections and mse notes

--- climate mse notes.R
+++ climate mse notes.R
@@ -1,6 +1,15 @@
+# -- Mean-R OMs
 # Computer 12 = HCRs 1/2
 # Computer 1 = HCR 3/4
-# Computer 3 = HCR 5 (6:8 cap, 5:8 no cap need to be done) Computer 11 is running HCR 5 for ricker OMs
-# Computer 4/10 = HCR 6 
+# Computer 3 = HCR 5 
+# Computer 4 = HCR 6 
 # Computer 5 = HCR 7
+# Computer 8 =  HCR 8
+
+# -- Ricker OMs
+# Computer 12 = HCRs 1/2
+# Computer 1 = HCR 3/4
+# Computer 11 = HCR 5 
+# Computer 4 = HCR 6 
+# Computer 10 = HCR 7
 # Computer 8 =  HCR 8
