46588512cd578177a43ad9f5142df114f6fb0e76
Grant Adams <grantadams60091@gmail.com>
2024-05-07 10:37:53 -0700

    Update climate mse notes.R

--- climate mse notes.R
+++ climate mse notes.R
@@ -5,14 +5,14 @@
 # Computer 12 = HCRs 1/2
 # Computer 1 = HCR 3/4
 # Computer 3 = HCR 5 
-# Computer 4 = HCR 6: being redone on 3 (no cap on 1)
-# Computer 5 = HCR 7
+# Computer 4 = HCR 6: (OMs 1-2 being redone on 8; OMs 3-4 being redone on 1 )
+# Computer 5 = HCR 7: 
 # Computer 8 =  HCR 8
 
 # -- Ricker OMs
 # Computer 12 = HCRs 1/2
 # Computer 1 = HCR 3/4
 # Computer 11 = HCR 5 
-# Computer 4 = HCR 6: being redone (cap on 10 & 3, no cap on 8 & 12)
-# Computer 10 = HCR 7
-# Computer 8 =  HCR 8
+# Computer 4 = HCR 6: being redone (cap 5-6 OM on 8, no cap on 5-6 OM on 10 & 7-8 OM on 12) 5/7/2024. Need to be taken off
+# Computer 10 = HCR 7: being redone on 11
+# Computer 8 =  HCR 8: being redone on 11
