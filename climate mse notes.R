330f869596f255efde6c64796a6e20b893cfde6e
Grant Adams <grantadams60091@gmail.com>
2024-05-08 21:08:51 -0700

    Update climate mse notes.R

--- climate mse notes.R
+++ climate mse notes.R
@@ -1,18 +1,19 @@
 # "ss_run_Tier3", "ss_run_dynamicTier3", "ss_run_M_Tier3", "ss_run_M_dynamicTier3"
 # "ms_run_fb40", "ms_run_fb40iter", "ms_run_cmsy", "ms_run_concmsy"
 
-# -- Mean-R OMs
-# Computer 12 = HCRs 1/2
-# Computer 1 = HCR 3/4
-# Computer 3 = HCR 5 
-# Computer 4 = HCR 6: (OMs 1-2 being redone on 8; OMs 3-4 being redone on 1 )
-# Computer 5 = HCR 7: 
-# Computer 8 =  HCR 8
 
-# -- Ricker OMs
-# Computer 12 = HCRs 1/2
-# Computer 1 = HCR 3/4
-# Computer 11 = HCR 5 
-# Computer 4 = HCR 6: being redone (cap 5-6 OM on 8, no cap on 5-6 OM on 10 & 7-8 OM on 12) 5/7/2024. Need to be taken off
-# Computer 10 = HCR 7: being redone on 11
-# Computer 8 =  HCR 8: being redone on 11
+# Currently running (5/8/12)
+# Computer 11: HCR 7 OMs 5-8  (No Cap & Cap)
+# Computer 10: HCR 6 - OMs 7:8 (No Cap & Cap)
+# Computer 12: HCR 6 - OMs 5:6 (No Cap & Cap)
+# Computer 8: HCR 6 - OMs 1:2 (No Cap & Cap)
+# Computer 1: HCR 6 - OMs 3:4 (No Cap & Cap)
+
+# - Need to be backed up (5/8/12)
+# Computer 8: HCR 6 - cap 5-6 OM
+# Computer 10: HCR 6 - no cap on 5-6 OM
+# Computer 12: HCR 6: no cap 7-8 OM 
+# Computer 1: HCR 6 - cap 1-2 OM 
+
+# Need reinstall Rceattle (5/8/12)
+# - Comps 11, 3, 4
