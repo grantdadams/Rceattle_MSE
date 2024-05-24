cd497a2e13a476a3e0b54bb0a75b471372f5f63a
Grant Adams <grantadams60091@gmail.com>
2024-05-24 14:04:02 -0700

    Update climate mse notes.R

--- climate mse notes.R
+++ climate mse notes.R
@@ -6,14 +6,19 @@
 
 # - Computer 4: HCR 5 OMs 5-8  (No Cap & Cap)
 
-# - Computer 8:  HCR 6 - OMs 1:2 (No Cap & Cap)
-# - Computer 12: HCR 6 - OMs 4:3 (No Cap & Cap)
-# - Computer 1:  HCR 6 - OMs 5:6 (No Cap & Cap)
 # - Computer 10: HCR 6 - OMs 7:8 (No Cap & Cap)
+# - Computer 12: HCR 6 - OMs 7:8 (Cap)
 
+# - Computer 1: HCR 6 - OMs 3:4  (No Cap)
+# - Computer 11: HCR 6 - OMs 3:4  (Cap)
 
 # - Computer 3: HCR 8 - OMs 5:8 (No Cap & Cap)
-# - Computer 11: HCR 8 OMs 5-8  (Cap)
+# - Computer 8: HCR 8 - OMs 5:8 (Cap)
+
 
 # Need to be re-run
-# HCR 5, 7, 8 OMs 1:4
+# HCR 5, 8 OMs 1:4
+
+# Done
+# HCR 6 - OMs 1:2, 5:6 (No Cap & Cap)
+# HCR 7 - OMs 5:8  (No Cap & Cap)
