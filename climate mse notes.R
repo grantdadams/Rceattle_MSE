e7af581a0bac5b3ed56a94cd63ff61e93692181d
Grant Adams <grantadams60091@gmail.com>
2024-06-14 11:04:27 -0700

    EM 7 - OMs 1:4

--- climate mse notes.R
+++ climate mse notes.R
@@ -2,23 +2,32 @@
 # "ms_run_fb40", "ms_run_fb40iter", "ms_run_cmsy", "ms_run_concmsy"
 
 
-# Currently running (5/8/12)
+# Currently running ----
 
-# - Computer 4: HCR 5 OMs 5-8  (No Cap & Cap)
+# - Computer 12: HCR 1:2 - OMs 1:8 (No Cap & Cap) SLOW
 
-# - Computer 10: HCR 6 - OMs 7:8 (No Cap & Cap)
-# - Computer 12: HCR 6 - OMs 7:8 (Cap)
+# - Computer 1: HCR 8 - OMs 1,5 (No Cap)... HCR 3:4 - OMs 1:8 (No Cap & Cap)
 
-# - Computer 1: HCR 6 - OMs 3:4  (No Cap)
-# - Computer 11: HCR 6 - OMs 3:4  (Cap)
+# - Computer 4: HCR 5 - OMs 1:4 (No Cap & Cap)
 
-# - Computer 3: HCR 8 - OMs 5:8 (No Cap & Cap)
-# - Computer 8: HCR 8 - OMs 5:8 (Cap)
+# - Computer 11: HCR 7 - OMs 1,5 (No Cap & Cap)
+
+# - Computer 8: HCR 8 - OMs 1,5 (Cap)
 
 
-# Need to be re-run
-# HCR 5, 8 OMs 1:4
+# Need to be re-run ----
 
-# Done
-# HCR 6 - OMs 1:2, 5:6 (No Cap & Cap)
-# HCR 7 - OMs 5:8  (No Cap & Cap)
+
+
+# Locations ----
+# HCR 5 - OMs 5:8 (No Cap & Cap): Computer 4
+
+# HCR 6 - OMs 1:2 (No Cap & Cap): Computer 8 & 4
+# HCR 6 - OMs 3:4 (No Cap & Cap): Computer 12
+# HCR 6 - OMs 5:6 (No Cap & Cap): Computer 1
+# HCR 6 - OMs 7:8 (No Cap & Cap): Computer 12
+
+# HCR 7 - OMs 1:8 (No Cap & Cap): Computer 11
+
+# HCR 8 - OMs 1:8 (No Cap): Computer 1
+# HCR 8 - OMs 1:8 (Cap): Computer 8
