a787565b393c09b65a5df8129d235feccef48a29
Grant.Adams <grantadams60091@gmail.com>
2026-03-19 12:05:06 -0700

    clean up to only use Tier 3 and removing GOA files

--- 1a_BSAI_MSE_Runs.R
+++ 1a_BSAI_MSE_Runs.R
@@ -24,31 +24,11 @@ om_names = c("MS_OM", "MS_Ricker_OM")
 
 # HCR
 # 1, NPFMC Tier 3 HCR
-# 1b. NPFMC Tier 3 dynamic HCR 
+em_hcr_list <- list(ss_run_Tier3,
+                    ss_run_M_Tier3) 
 
-
-# GRANT COMMENTED OUT (3/19/2026):
-# 2. PFMC Category 1 40-10 HCR
-# 2b. PFMC Category 1 40-10 dynamic HCR
-# 3. SESSF Tier 1 HCR
-# 3b. SESSF Tier 1 dynamic HCR
-# 4. Average F
-# 5. Equilibrium F_(40%)
-em_hcr_list <- list(ss_run_Tier3, ss_run_dynamicTier3,
-                    ss_run_M_Tier3, ss_run_M_dynamicTier3) 
-
-
-# GRANT COMMENTED OUT (3/19/2026):
-# , ss_run_Cat1, ss_run_dynamicCat1, ss_run_Tier1, ss_run_dynamicTier1, ss_run_Fspr, ss_run_AvgF, # Fixed M
-# ss_run_M_Tier3, ss_run_M_dynamicTier3, ss_run_M_Cat1, ss_run_M_dynamicCat1, ss_run_M_Tier1, ss_run_M_dynamicTier1, ss_run_M_Fspr, ss_run_M_AvgF) # Estimate 
-
-em_hcr_names <- c("SS_fixM_Tier3_EM", "SS_fixM_dynamicTier3_EM",
-                  "SS_estM_Tier3_EM", "SS_estM_dynamicTier3_EM") 
-
-# GRANT COMMENTED OUT (3/19/2026):                  # 
-# "SS_fixM_Cat1_EM", "SS_fixM_dynamicCat1_EM", "SS_fixM_Tier1_EM", "SS_fixM_dynamicTier1_EM", "SS_fixM_Fspr_EM", "SS_fixM_AvgF_EM", # Fixed M
-# "SS_estM_Tier3_EM", "SS_estM_dynamicTier3_EM", "SS_estM_Cat1_EM", "SS_estM_dynamicCat1_EM", "SS_estM_Tier1_EM", "SS_estM_dynamicTier1_EM", "SS_estM_Fspr_EM", "SS_estM_AvgF_EM")
-
+em_hcr_names <- c("SS_fixM_Tier3_EM",
+                  "SS_estM_Tier3_EM") 
 
 ### Run the MSE
 source("R/Run_MSE_function.R")
